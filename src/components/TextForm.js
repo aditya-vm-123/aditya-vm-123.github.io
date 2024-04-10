74a03760ecc121ac51efe1c77304040e0198ebdb
Natsuwuki <natsukikai1@gmail.com>
2024-04-10 13:33:32 +0530

    Latest

---
+++ src/components/TextForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        // console.log("Uppercase was clicked: " + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleLowClick = () => {
+        // console.log("Lowercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleOnChange = (event) => {
+        // console.log("On change");
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState('Enter text here2');
+
+    return (
+        <div>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea   textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+            &emsp;
+            <button className="btn btn-primary" onClick={handleLowClick}>Convert to lowercase</button>
+        </div>
+  )
+}
